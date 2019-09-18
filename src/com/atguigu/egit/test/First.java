58d12a0847c8c467d6b06d211a1743c8d2a8f32e
zhaoSir <1192338680@qq.com>
2019-09-18 23:16:57 +0800

    first3

--- src/com/atguigu/egit/test/First.java
+++ src/com/atguigu/egit/test/First.java
@@ -3,6 +3,7 @@ package com.atguigu.egit.test;
 public class First {
 
 	public First() {
+		System.out.println("成功");
 		// TODO Auto-generated constructor stub
 		// TODO Auto-generated constructor stub
 		// TODO Auto-generated constructor stub
